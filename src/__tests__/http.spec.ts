80e45bef321b6f127f13f308b314527b63004f71
Shyam-Chen <shyamchen1994@gmail.com>
2023-08-01 09:33:07 +0800

    6th Commit

---
+++ src/__tests__/http.spec.ts
@@ -0,0 +1,49 @@
+import type { FastifyInstance } from 'fastify';
+import { beforeEach, test, expect } from 'vitest';
+import fastify from 'fastify';
+
+import { serverFactory } from '../fastify-uws';
+
+let app: FastifyInstance;
+
+beforeEach(() => {
+  app = fastify({ serverFactory });
+});
+
+test('HTTP - GET', async () => {
+  app.register(
+    async (router) => {
+      router.get('', async (req, reply) => {
+        return reply.send({
+          message: 'Hello, World!',
+        });
+      });
+    },
+    { prefix: '/hello-http' },
+  );
+
+  const res = await app.inject({ method: 'GET', url: '/hello-http' });
+
+  expect(res.json()).toEqual({ message: 'Hello, World!' });
+});
+
+test('HTTP - POST', async () => {
+  app.register(
+    async (router) => {
+      router.post<{ Body: { text: string } }>('', async (req, reply) => {
+        return reply.send({
+          message: req.body.text,
+        });
+      });
+    },
+    { prefix: '/hello-http' },
+  );
+
+  const res = await app.inject({
+    method: 'POST',
+    url: '/hello-http',
+    body: { text: 'Hello, World!' },
+  });
+
+  expect(res.json()).toEqual({ message: 'Hello, World!' });
+});
