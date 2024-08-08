a53b1e5df13de9b61049ccb0ba041a266803c6c2
Shyam-Chen <shyamchen1994@gmail.com>
2024-08-08 11:58:25 +0800

    74th Commit

--- client-fd.mjs
+++ client-fd.mjs
@@ -4,7 +4,7 @@ import FormData from 'form-data';
 
 async function upload() {
   const formData = new FormData();
-  formData.append('image', fs.createReadStream('./fastify.png'));
+  formData.append('image', fs.createReadStream('./.github/assets/logo.png'));
 
   const response = await axios.post('http://127.0.0.1:3000/api/hello-fd', formData, {
     headers: { ...formData.getHeaders() },
