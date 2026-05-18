0c74a0857dfdbdfc9e2fb5cbd8b1532fbbb93df7
Shyam-Chen <shyamchen1994@gmail.com>
2026-05-18 16:39:03 +0800

    100th Commit

--- client-es.mjs
+++ client-es.mjs
@@ -5,3 +5,14 @@ const es = new EventSource('http://127.0.0.1:3000/api/hello-sse');
 es.addEventListener('message', (evt) => {
   console.log(evt.data);
 });
+
+// const es = new EventSource('http://127.0.0.1:3000/api/hello-sse/count');
+
+// es.addEventListener('message', (evt) => {
+//   console.log(evt.data);
+// });
+
+// es.addEventListener('end', () => {
+//   console.log('Done');
+//   es.close();
+// });
--- examples/src/routes/hello-sse/+handler.ts
+++ examples/src/routes/hello-sse/+handler.ts
@@ -11,4 +11,28 @@ export default (async (app) => {
       app.log.info('Client disconnected');
     });
   });
+
+  app.get('/count', { sse: true }, async (request, reply) => {
+    app.log.info('Client connected');
+
+    async function* counter() {
+      const count = 3;
+
+      for (let i = 1; i <= count; i++) {
+        yield { data: String(i) };
+
+        if (i < count) {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+      }
+    }
+
+    await reply.sse.send(counter());
+
+    await reply.sse.send({ event: 'end', data: null });
+
+    request.raw.on('close', () => {
+      app.log.info('Client disconnected');
+    });
+  });
 }) as FastifyPluginAsyncTypebox;
